dc52cf70e2cbc6d4910db007542cb19212e7a0da
eric <ericlempe1994@gmail.com>
2020-12-29 16:02:13 -0300

    functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,25 @@
-const myConst = 5;
+// var myList = [
+//     {a: 10},
+//     {a: 15},
+//     {a: 25}
+// ];
+//
+// var myNewList = myList.map( obj => obj.a )
+//
+// console.log(myNewList);
 
-console.log(myConst);
+var a = 11;
+
+var myobj = {
+    a: 10,
+    myFunc:  () => {
+        console.log(this.a);
+        var newFunc = () => {
+            this.a += 5;
+            console.log(this.a);
+        }
+        newFunc();
+    }
+};
+
+myobj.myFunc();
